6953257196794d074c4cdc11a9a426077c497657
Abdurehman <abdurehmanali611@gmail.com>
2026-08-27 03:29:09 +0300

    final

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api";
+  process.env.NEXT_PUBLIC_API_URL || "https://mediflow-backend-delta.vercel.app/api";
 
 export const api = axios.create({
   baseURL: API_URL,
